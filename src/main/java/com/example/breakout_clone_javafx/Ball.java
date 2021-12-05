c44f4f6b7eb1e21c4cf17b59c72a352bf6d200eb
Praket Chavan <praket.chavan@gmail.com>
2021-12-05 16:24:21 +0000

    refact: Refactored the code to allow the multiple ball objects in the game
    Changed the speed to be non static so that balls do not share the same speed vector
    
    Signed-off-by: Praket Chavan <praket.chavan@gmail.com>

--- src/main/java/com/example/breakout_clone_javafx/Ball.java
+++ src/main/java/com/example/breakout_clone_javafx/Ball.java
@@ -3,34 +3,37 @@ package com.example.breakout_clone_javafx;
 import com.example.util.CircleSprite;
 import com.example.util.Vector2D;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Region;
-
 public abstract class Ball extends CircleSprite {
-    private static final SimpleDoubleProperty HEIGHT = new SimpleDoubleProperty(25);
-    private static final SimpleDoubleProperty WIDTH = new SimpleDoubleProperty(25);
+    private static final SimpleDoubleProperty HEIGHT = new SimpleDoubleProperty(
+            25);
+    private static final SimpleDoubleProperty WIDTH = new SimpleDoubleProperty(
+            25);
     private static final SimpleDoubleProperty DEF_RADIUS =
             new SimpleDoubleProperty(12.5);
-    private static final SimpleObjectProperty<Vector2D> DEF_BALL_SPEED =
-            new SimpleObjectProperty<>(new Vector2D(1, 1));
+    private static final double DEF_INITIAL_SPEED_X = 1;
+    private static final double DEF_INITIAL_SPEED_Y = 1;
 
 
     public Ball(SimpleObjectProperty<Vector2D> position) {
-        super(DEF_BALL_SPEED, position, DEF_RADIUS, HEIGHT, WIDTH);
+        super(new SimpleObjectProperty<>(
+                      new Vector2D(DEF_INITIAL_SPEED_X, DEF_INITIAL_SPEED_Y)),
+              position, DEF_RADIUS, HEIGHT, WIDTH);
     }
+
 
     @Override
     public void move() {
+        onImpact(findImpact(null));
         getPosition().addVector(getSpeed());
         display();
     }
 
     @Override
     public void display() {
-       relocate(getPosition().getX(), getPosition().getY());
-       setRotate(0);
+        relocate(getPosition().getX() - this.getRadius(),
+                 getPosition().getY() - this.getRadius());
+        setRotate(0);
     }
 }
