f3fc1e12dd822b6b69ed084d36f5fcd0b08b6465
Praket Chavan <psypc1@nottingham.ac.uk>
2021-12-21 12:02:03 +0000

    feature a new wrapper class for a ball type
    Create a new wrapper class for the Rubberball to encapsulate the MVC
    classes for the ball.

---
+++ src/main/java/com/example/migrateFx/wrappers/RubberBall.java
@@ -0,0 +1,35 @@
+package com.example.migrateFx.wrappers;
+
+import com.example.migrateFx.controller.BallController;
+import com.example.migrateFx.model.BallModel;
+import com.example.migrateFx.view.BallView;
+import javafx.geometry.Point2D;
+
+
+public class RubberBall {
+    private static final double BALL_SIZE = 20;
+    private final BallModel m_Model;
+    private final BallController m_Controller;
+    private final BallView m_View;
+
+    public BallController getController() {
+        return m_Controller;
+    }
+
+    public BallModel getModel() {
+        return m_Model;
+    }
+
+    public BallView getView() {
+        return m_View;
+    }
+
+    public RubberBall(String url, Point2D location) {
+        m_Model = new BallModel(location);
+        m_View = new BallView(url, BALL_SIZE, BALL_SIZE);
+        m_Controller = new BallController(getModel(), getView());
+        getController().initialize();
+    }
+
+
+}
