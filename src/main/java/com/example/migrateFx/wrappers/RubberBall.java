258791d41223b2de29eac17b99de9d638fb39ad9
Praket Chavan <psypc1@nottingham.ac.uk>
2021-12-23 11:01:53 +0000

    Refactor the rubberball wrapper class
    Pushed up member variable and their initialisation to the general
    wrapper class

--- src/main/java/com/example/migrateFx/wrappers/RubberBall.java
+++ src/main/java/com/example/migrateFx/wrappers/RubberBall.java
@@ -6,31 +6,11 @@ import com.example.migrateFx.view.BallView;
 import javafx.geometry.Point2D;
 
 
-public class RubberBall {
+public class RubberBall extends Ball{
     private static final double BALL_SIZE = 20;
-    private final BallModel m_Model;
-    private final BallController m_Controller;
-    private final BallView m_View;
-
-    public BallController getController() {
-        return m_Controller;
-    }
-
-    public BallModel getModel() {
-        return m_Model;
-    }
-
-    public BallView getView() {
-        return m_View;
-    }
 
     public RubberBall(String url, Point2D location) {
-        m_Model = new BallModel(location);
-        m_View = new BallView(url, BALL_SIZE, BALL_SIZE);
-        m_Controller = new BallController(getModel(), getView());
-        getController().initialize();
+        super(url, location, BALL_SIZE);
     }
-
-
 
 }
