ff6cbd8f0e2690c0e92461554fa1ecfd8dd28e95
Praket Chavan <psypc1@nottingham.ac.uk>
2021-12-24 12:23:29 +0000

    Feature: new classes for different types of bricks
    Add classes for different types of bricks

---
+++ src/main/java/com/example/migrateFx/wrappers/SteelBrick.java
@@ -0,0 +1,9 @@
+package com.example.migrateFx.wrappers;
+
+import javafx.geometry.Point2D;
+
+public class SteelBrick extends Brick{
+    public SteelBrick(String url, Point2D location) {
+        super(url, location);
+    }
+}
---
+++ src/main/java/com/example/migrateFx/wrappers/UnbreakableBrick.java
@@ -0,0 +1,9 @@
+package com.example.migrateFx.wrappers;
+
+import javafx.geometry.Point2D;
+
+public class UnbreakableBrick extends Brick{
+    public UnbreakableBrick(String url, Point2D location) {
+        super(url, location);
+    }
+}
