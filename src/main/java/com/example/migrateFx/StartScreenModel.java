62481f68f3bebb57abc76475f80e290f5e445052
Praket Chavan <psypc1@nottingham.ac.uk>
2021-12-30 20:01:32 +0000

    Refactor the code to remove unused resoures
    Remove the unused classes and resources from the module

--- src/main/java/com/example/migrateFx/StartScreenModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.migrateFx;
-
-import javafx.beans.property.StringProperty;
-
-public class StartScreenModel {
-    private  StringProperty Title;
-}
--- src/main/java/com/example/migrateFx/model/ChessBoardLevelModel.java
+++
@@ -1,4 +0,0 @@
-package com.example.migrateFx.model;
-
-public class ChessBoardLevelModel {
-}
